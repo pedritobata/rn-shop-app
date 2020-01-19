3fe9362904c8cbf66ca8f568d4b285dac5fce470
unknown <pedromartinez_goyo@hotmail.com>
2020-01-18 20:07:33 -0500

    input validation in editing/creating screens finished

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -6,13 +6,15 @@ import {
   TextInput,
   StyleSheet,
   Platform,
-  Alert
+  Alert,
+  KeyboardAvoidingView 
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import * as productsActions from '../../store/actions/products';
+import Input from '../../components/UI/Input';
 
 
 const FORM_INPUT_UPDATE = 'FORM_INPUT_UPDATE';
@@ -97,67 +99,78 @@ const EditProductScreen = props => {
   }, [submitHandler]);
 
 
-  const textChangeHandler = (inputIdentifier,text) => {
-    let isValid = false;
-    if(text.trim().length > 0){
-      isValid = true;
-    }
-    dispatchFormState({
-      type: FORM_INPUT_UPDATE, 
-      value: text,
-      isValid: isValid,
-      input: inputIdentifier
-    });
-  }
+  const inputChangeHandler = useCallback((inputIdentifier,inputValue, inputValidity) => {
+      dispatchFormState({
+        type: FORM_INPUT_UPDATE, 
+        value: inputValue,
+        isValid: inputValidity,
+        input: inputIdentifier
+      });
+  }, [dispatchFormState]);
 
   return (
-    <ScrollView>
-      <View style={styles.form}>
-        <View style={styles.formControl}>
-          <Text style={styles.label}>Title</Text>
-          <TextInput
-            style={styles.input}
-            value={formState.inputValues.title}
-            onChangeText={textChangeHandler.bind(this, 'title')}
+    <KeyboardAvoidingView style={{flex: 1}} behavior="padding" keyboardVerticalOffset={70}>
+      <ScrollView>
+        <View style={styles.form}>
+          <Input 
+            label="Title"
             keyboardType="default"
+            errorText="Please enter a valid title!"
             autoCapitalize="sentences"
             autoCorrect
             returnKeyType="next"//solo dice qué apariencia tendrá el boton de enter del teclado
-            //esto se dispara cuando hemos escrito algo en el campo y damos touch en cualquier otro lado
-            onEndEditing={() => console.log("onEndEditing")}
-            //esto se dispara cuando presionamos el boton de enter
-            onSubmitEditing={() => console.log("onSubmitEditing")}
+            // si uso bind, origino que inputChangeHandler se recree en cada letra que tecleo
+            //lo cual originará que hayan muchos rerender en el componente Input y el comportamiento
+            // de la logica de validacion se va a la mierda!!
+           /*  onInputChange={inputChangeHandler.bind(this, 'title')} */
+           //usamos sin bind. para esto agregamos un prop extra llamado id donde mandaremos el identifier
+            onInputChange={inputChangeHandler}
+            id="title"
+            initialValue={editedProduct ? editedProduct.title : ''}
+            initiallyValid={!!editedProduct}//doble !! es para enviar true o false y NO el valor de initiallyValid en sí
+            required
+          />
+          <Input 
+            label="Image Url"
+            errorText="Please enter a valid url!"
+            keyboardType="default"
+            returnKeyType="next"
+            onInputChange={inputChangeHandler}
+            id="imageUrl"
+            initialValue={editedProduct ? editedProduct.imageUrl : ''}
+            initiallyValid={!!editedProduct}
+            required
+          />
+          {editedProduct ? null : (
+             <Input 
+              label="Price"
+              errorText="Please enter a valid price!"
+              keyboardType="decimal-pad"
+              returnKeyType="next"
+              onInputChange={inputChangeHandler}
+              id="price"
+              required
+              min={0.1}
+           />
+          )}
+          <Input 
+            label="Description"
+            errorText="Please enter a valid description!"
+            keyboardType="default"
+            autoCapitalize="sentences"
+            autoCorrect
+            multiline
+            numberOfLines={3}//solo funca para android
+            initialValue={editedProduct ? editedProduct.description : ''}
+            initiallyValid={!!editedProduct}
+            onInputChange={inputChangeHandler}
+            id="description"
+            required
+            minLength={5}
           />
         </View>
-        <View style={styles.formControl}>
-          <Text style={styles.label}>Image URL</Text>
-          <TextInput
-            style={styles.input}
-            value={formState.inputValues.imageUrl}
-            onChangeText={textChangeHandler.bind(this, 'imageUrl')}
-          />
-        </View>
-        {editedProduct ? null : (
-          <View style={styles.formControl}>
-            <Text style={styles.label}>Price</Text>
-            <TextInput
-              style={styles.input}
-              value={formState.inputValues.price}
-              onChangeText={textChangeHandler.bind(this, 'price')}
-              keyboardType="number-pad"
-            />
-          </View>
-        )}
-        <View style={styles.formControl}>
-          <Text style={styles.label}>Description</Text>
-          <TextInput
-            style={styles.input}
-            value={formState.inputValues.description}
-            onChangeText={textChangeHandler.bind(this, 'description')}
-          />
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView >
   );
 };
 
@@ -185,19 +198,7 @@ const styles = StyleSheet.create({
   form: {
     margin: 20
   },
-  formControl: {
-    width: '100%'
-  },
-  label: {
-    fontFamily: 'open-sans-bold',
-    marginVertical: 8
-  },
-  input: {
-    paddingHorizontal: 2,
-    paddingVertical: 5,
-    borderBottomColor: '#ccc',
-    borderBottomWidth: 1
-  }
+  
 });
 
 export default EditProductScreen;
